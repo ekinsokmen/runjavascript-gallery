60f64edef9425515f7c98957e64dc52b092c45ea
Ekin Sokmen <ekin.sokmen@gmail.com>
2018-05-11 00:07:43 +0100

    TFL Bus Script - Close button

--- samples/tfl/tfl-bus.js
+++ samples/tfl/tfl-bus.js
@@ -283,7 +283,7 @@ async function displayTimeTable(timeTableDataPromise) {
     var minStr = calcActualTimeToStationMinutesStr(mins);
     output += "<div class='depb'><span class='left'>" + t.line + "</span><span id='tti_" + i + "' class='right'>" + minStr + "</span></div>";
   });
-  output = "<center onclick='runjs.close()'>" + output + "</center>";
+  output = "<center>" + output + "</center>";
   var reloadButton = `
   <svg id='reloadButton' onclick="runjs.custom.reload()" style="color: white;margin-top: 10px;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-refresh-cw">
   <polyline points="23 4 23 10 17 10"></polyline><polyline points="1 20 1 14 7 14"></polyline>
@@ -295,7 +295,15 @@ async function displayTimeTable(timeTableDataPromise) {
   <line x1="12" y1="2" x2="12" y2="6"></line><line x1="12" y1="18" x2="12" y2="22"></line><line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line><line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line><line x1="2" y1="12" x2="6" y2="12"></line><line x1="18" y1="12" x2="22" y2="12"></line><line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line><line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
   </svg>
   `
-  output += "<center>" + reloadButton + reloadingImg + "</center>";
+  var closeButton=`
+  <div style="position: fixed;bottom: 5px;left: 90%; opacity: 1.0;">
+    <small>
+      <svg xmlns="http://www.w3.org/2000/svg" style="color: white;" onclick="runjs.close()" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
+    </small>
+  </div>
+  `
+
+  output += "<center>" + reloadButton + reloadingImg + "</center>" + closeButton;
   document.getElementsByTagName("body")[0].innerHTML=output;
   setTimeout(updateTimeTable, 1000);
 }
