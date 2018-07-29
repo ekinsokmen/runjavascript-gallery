7ca8742b1417e88e1049b066bc802c042899fb13
Ekin Sokmen <ekin.sokmen@gmail.com>
2018-07-29 13:54:32 +0100

    Decimal fix for 100%.

--- how-to/integration/workflow/battery-meter/battery-meter.js
+++ how-to/integration/workflow/battery-meter/battery-meter.js
@@ -1,7 +1,7 @@
 var template=`
 <div style="margin-top: 50px;">
 <h1 style="position: absolute; margin-left: 45%; margin-top: 7px; font-family: helvetica; font-size: 30px;">##P##%</h1>
-<meter value="0.##P##" style="width: 80%;height: 50px; margin: 0px 10% 0px 10%;"></meter>
+<meter value="##DP##" style="width: 80%;height: 50px; margin: 0px 10% 0px 10%;"></meter>
 </div>
 `;
 
@@ -9,5 +9,10 @@ runjs.openXCallBackURL(
   'workflow://x-callback-url/run-workflow?name=GetBatteryLevel',
 'result'
 ).then(battery => {
-  runjs.printHTML(template.replace(/##P##/g, battery));
+  var decimalLevel = parseInt(battery)/100;
+  runjs.printHTML(
+    template
+      .replace(/##P##/g, battery)
+      .replace(/##DP##/g, decimalLevel)
+  );
 });
